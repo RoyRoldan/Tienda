ff1581050d0145135ea9f53be33700581c409e3d
RoyRoldan <112263729+RoyRoldan@users.noreply.github.com>
2023-11-08 17:18:15 -0600

    Finalizacion Semana 09

--- src/main/java/com/Tienda/service/ProductoService.java
+++ src/main/java/com/Tienda/service/ProductoService.java
@@ -1,19 +1,24 @@
-
 package com.Tienda.service;
 
 import com.Tienda.domain.Producto;
 import java.util.List;
 
-
 public interface ProductoService {
-    
-    
+
     public List<Producto> getProductos(boolean activos);
-    
+
     public Producto getProducto(Producto producto);
-    
+
     public void save(Producto producto);
-    
+
     public void delete(Producto producto);
+
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+
+    //Lista de productos utilizando consultas con JPQL    
+    public List<Producto> metodoJPQL(double precioInf, double precioSup);
     
+     //Lista de productos utilizando consultas con SQL Nativo
+    public List<Producto> metodoNativo(double precioInf, double precioSup);
 }
--- src/main/java/com/Tienda/service/impl/ProductoServiceImpl.java
+++ src/main/java/com/Tienda/service/impl/ProductoServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.Tienda.service.impl;
 
 import com.Tienda.dao.ProductoDao;
@@ -12,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class ProductoServiceImpl implements ProductoService {
 
-
     @Autowired
     private ProductoDao productoDao;
 
@@ -20,7 +18,7 @@ public class ProductoServiceImpl implements ProductoService {
     @Transactional(readOnly = true)
     public List<Producto> getProductos(boolean activos) {
         List<Producto> productos = productoDao.findAll();
-        var lista=productoDao.findAll();
+        var lista = productoDao.findAll();
         if (activos) {
             productos.removeIf(p -> !p.isActivo());
         }
@@ -32,7 +30,7 @@ public class ProductoServiceImpl implements ProductoService {
     public Producto getProducto(Producto producto) {
         return productoDao.findById(producto.getIdProducto()).orElse(null);
     }
-    
+
     @Override
     @Transactional
     public void save(Producto producto) {
@@ -43,5 +41,24 @@ public class ProductoServiceImpl implements ProductoService {
     @Transactional
     public void delete(Producto producto) {
         productoDao.delete(producto);
+    }
+
+    // Lista de productos con precio entre ordendados por descripción ConsultaAmpliada
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup) {
+        return productoDao.findByPrecioBetweenOrderByDescripcion(precioInf, precioSup);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> metodoJPQL(double precioInf, double precioSup) {
+        return productoDao.metodoJPQL(precioInf, precioSup);
+    }
+    
+    @Override
+    @Transactional(readOnly=true)    
+    public List<Producto> metodoNativo(double precioInf, double precioSup) {
+        return productoDao.metodoNativo(precioInf, precioSup);
     }
 }
